f6c5a697fdef5a754eb069f529bb0893d9e0c09a
jstivel <stivel275@gmail.com>
2025-08-11 19:51:10 -0500

    se actualiza vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
+// import { fileURLToPath, URL } from 'node:url'
+//
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+//
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+// })
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // *** Línea añadida para GitHub Pages ***
+  // Reemplaza <nombre-del-repositorio> con el nombre de tu repositorio.
+  base: '/Dico-Liberty-frontend/',
+  // ***************************************
   plugins: [
     vue(),
     vueDevTools(),
